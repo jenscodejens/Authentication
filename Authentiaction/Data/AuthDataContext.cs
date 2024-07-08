ab95325e28687abeebe33c9831f9434d3cd5a42f
jenscodejens <161465689+jenscodejens@users.noreply.github.com>
2024-07-08 14:55:54 +0200

    Add project files.

---
+++ Authentiaction/Data/AuthDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Data
+{
+    public class AuthDataContext(DbContextOptions<AuthDataContext> options) : IdentityDbContext(options)
+    {
+    }
+}
+
+// Default login. Username: string@string.com Password: String1!
